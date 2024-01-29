602c6b46ebc892e2dd7d900715e73898b1182953
Nikola84ca <nicolateotorriatte@outlook.com>
2024-01-29 22:07:52 +0000

    implemented the routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./index.css";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import { Routes , Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      <h1>This is my Portfolio</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
